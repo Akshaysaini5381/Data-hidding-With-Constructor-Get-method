998c335c333544b2294728c32dc76980cf288a3d
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-24 23:46:30 +0530

    Add project files.

---
+++ Data hidding With Constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_hidding_With_Constructor
+{
+
+    class data
+    {
+        private int num;
+
+    public    data(int num)
+        {
+            this.num = num;
+        }
+        public int GetNum()
+        {
+            return this.num;
+        }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            data obj = new data(100);
+            int num1 = obj.GetNum();
+            Console.WriteLine(num1);
+            Console.ReadLine();
+        }
+    }
+}
